2f00a60d1546ddfe0affe7e363992584aebe4b79
PAUL DUMITRU <dumitrupaul18@yahoo.com>
2022-11-16 20:25:33 +0200

    v.03

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # SimpleCandyCrush
 Candy Crush using JavaScript HTML and CSS
 aham
+
+-usor cu hate-ul 
--- candy.js
+++
@@ -1,4 +0,0 @@
-var candies = ["blue","purple,"red","yellow","orange","green"];
-var board =[];
-var rows = 12;
-var column =12;
